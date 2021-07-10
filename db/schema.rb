e17d6d2d2e969e87eafcc172c0e97b7e064ed249
shumpei116 <>
2021-07-10 13:19:51 +0900

    userテーブル変更の為migrateファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,9 @@
 ActiveRecord::Schema.define(version: 2021_07_10_035049) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
+    t.text "introduction"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
