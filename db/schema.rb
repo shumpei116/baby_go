2f448b5d6ed701139db237b2a804bd430b2b6e22
shumpei116 <>
2021-07-13 06:22:40 +0900

    carrierwaveのセットアップと画像表示の変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_035049) do
+ActiveRecord::Schema.define(version: 2021_07_12_203421) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_07_10_035049) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
