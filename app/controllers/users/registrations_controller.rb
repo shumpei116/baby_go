3c11c7710714667cff75e6eb042bc0faf677e923
shumpei116 <>
2021-07-14 06:16:09 +0900

    carrierwaveのホワイトリスト・リサイズの設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -51,7 +51,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduction, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduction, :avatar, :avatar_cache, :remove_avatar])
   end
 
   # The path used after sign up.
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,5 +1,5 @@
 class AvatarUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
+  include CarrierWave::MiniMagick
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
@@ -15,10 +15,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    # For Rails 3.1+ asset pipeline compatibility:
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.jpg"].compact.join('_'))
+  
+    # "/images/fallback/" + [version_name, "default.jpg"].compact.join('_')
   # end
 
   # Process files as they are uploaded:
@@ -29,15 +29,25 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  process resize_to_fit: [800, 800]
+
+  version :thumb do
+    process resize_and_pad: [300, 200]
+  end
+
+  version :medium_thumb do
+    process resize_and_pad: [250, 167]
+  end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_allowlist
   #   %w(jpg jpeg gif png)
   # end
+
+  def content_type_whitelist
+    /image\//
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
