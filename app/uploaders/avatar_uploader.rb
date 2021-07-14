20eabbbb38b7a5fae52ec5731b5a4369b8542bb0
shumpei116 <>
2021-07-15 05:13:48 +0900

    carrirewaveエラーメッセージ日本語化の為、uploaderとlocalesファイルを編集

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -45,9 +45,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_allowlist
+    %w(jpg jpeg gif png)
+  end
 
   def content_type_whitelist
     /image\//
