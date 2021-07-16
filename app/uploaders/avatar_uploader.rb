d3d3e140219fd791114a498788a477fd38fcd715
shumpei116 <>
2021-07-17 08:02:14 +0900

    不要なコメントの削除、行末改行の整理

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,9 +1,6 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
   if Rails.env.development?
     storage :file
   elsif Rails.env.test?
@@ -12,27 +9,14 @@ class AvatarUploader < CarrierWave::Uploader::Base
     storage :fog
   end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
-    # For Rails 3.1+ asset pipeline compatibility:
-    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.jpg"].compact.join('_'))
     "/images/fallback/" + [version_name, "default.jpg"].compact.join('_')
   end
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
   process resize_to_fit: [800, 800]
 
   version :thumb do
@@ -47,8 +31,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
     1..5.megabytes
   end
 
-  # Add an allowlist of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_allowlist
     %w(jpg jpeg gif png)
   end
@@ -56,10 +38,4 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def content_type_whitelist
     /image\//
   end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
   }
   resources :users, only: [:index, :show]
-
 end
