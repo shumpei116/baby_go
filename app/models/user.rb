0a3a4b7b7400a82c4e5fa5c82dcdb27e12e314a8
shumpei116 <>
2021-07-22 17:30:18 +0900

    storeモデルとのアソシエーション追加の為、userモデルにhas_manyを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   mount_uploader :avatar, AvatarUploader
 
+  has_many :stores, dependent: :destroy
+
   validates :name, presence: true, length: { maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
