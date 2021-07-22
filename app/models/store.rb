8a16b694176280a7f076fc171eaa62a402494ed7
shumpei116 <>
2021-07-22 17:28:58 +0900

    storeモデルにバリデーションを追加

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,12 @@
 class Store < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :introduction, presence: true, length: { maximum: 140 }
+  VALID_POSTCODE_REGEX = /\A\d{3}\d{4}\z/.freeze
+  validates :postcode, presence: true, format: { with: VALID_POSTCODE_REGEX }
+  validates :prefecture_code, presence: true
+  validates :city, presence: true, uniqueness: true
+  VALID_URL_REGEX = /https?:\/{2}[\w\/:%#\$&\?\(\)~\.=\+\-]+/.freeze
+  validates :url, format: { with: VALID_URL_REGEX }
 end
