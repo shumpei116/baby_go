f3569cace3262335e7b5977a764602dea16e61a5
shumpei116 <>
2021-07-22 18:13:24 +0900

    userモデルとのアソシエーション記述を変更

--- spec/factories/stores.rb
+++ spec/factories/stores.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :store do
-    user { create(:user) }
+    association :user
     name { "赤ちゃんの本舗" }
     introduction { "授乳室とおむつ交換スペースが完備！　綺麗で広くてとっても利用しやすいです" }
     postcode { "3120005" }
