5e0a0231ae5936c9d2d45b9815d402c9116f48e5
shumpei116 <>
2021-07-24 09:46:27 +0900

    ログインしていなくてもusers#showにアクセスできるように、before_actionを削除

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,33 +11,16 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'GET #show' do
-    context 'ユーザーがログインしているとき' do
-      before do
-        sign_in(user)
-        get user_path(user)
-      end
-
-      it '200レスポンスが返ってくること' do
-        expect(response).to have_http_status(200)
-      end
-
-      it '名前が含まれていること' do
-        expect(response.body).to include 'shumpei'
-      end
+    before do
+      get user_path(user)
     end
 
-    context 'ユーザーがログインしていないとき' do
-      before do
-        get user_path(user)
-      end
+    it '200レスポンスが返ってくること' do
+      expect(response).to have_http_status(200)
+    end
 
-      it '302レスポンスが返ってくること' do
-        expect(response).to have_http_status(302)
-      end
-
-      it 'ログインページにリダイレクトされること' do
-        expect(response).to redirect_to(new_user_session_path)
-      end
+    it '名前が含まれていること' do
+      expect(response.body).to include 'shumpei'
     end
   end
 end
