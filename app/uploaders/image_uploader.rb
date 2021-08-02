8eb898d95868b7ee2a13b281c2e559793813c4bd
shumpei116 <>
2021-08-03 06:01:17 +0900

    施設画像登録の為、image_uploader.rbを設定

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,37 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def default_url(*args)
+    "/images/fallback/" + [version_name, "default.jpg"].compact.join('_')
+  end
+
+  process resize_to_fit: [800, 800]
+
+  version :thumb do
+    process resize_and_pad: [300, 200]
+  end
+
+  version :medium_thumb do
+    process resize_and_pad: [250, 167]
+  end
+
+  def size_range
+    1..5.megabytes
+  end
+
+  def extension_allowlist
+    %w(jpg jpeg gif png)
+  end
+end
