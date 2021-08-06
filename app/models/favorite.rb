202982cd67edc00668edf81f3db72cc75f90ea66
shumpei116 <>
2021-08-06 14:52:50 +0900

    一意性のバリデーション追加の為、models/favorite.rbを編集

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,9 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :store
+
+  validates :user_id, presence: true
+  validates :store_id, presence: true
+
+  validates_uniqueness_of :store_id, scope: :user_id
 end
