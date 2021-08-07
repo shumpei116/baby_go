5ad861f1691173c7538da4d2efb26f8bc0df4725
shumpei116 <>
2021-08-08 07:17:17 +0900

    ユーザーがいいねした施設を取得する為、favorite_storesのアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :stores, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorite_stores, through: :favorites, source: :store
 
   validates :name, presence: true, length: { maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
