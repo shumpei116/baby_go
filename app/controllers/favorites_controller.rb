da8d3200814950f1d3f4d5dab6e5fda3f4c9dc7b
shumpei116 <>
2021-08-07 10:37:52 +0900

    favoritesコントローラーにcreate_destroyアクションを追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,18 @@
 class FavoritesController < ApplicationController
+  before_action :find_store
+
   def create
-    @favorite = current_user.favorites.create(store_id: params[:store_id])
+    @favorite = current_user.favorites.create(store_id: @store.id)
   end
 
   def destroy
-    @favorite = current_user.favorites.find_by(store_id: params[:store_id])
+    @favorite = current_user.favorites.find_by(store_id: @store.id)
     @favorite.destroy
+  end
+
+  private
+
+  def find_store
+    @store = Store.find(params[:store_id])
   end
 end
