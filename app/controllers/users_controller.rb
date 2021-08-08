64763372cfae23649c73cfb53735a27da9012a5a
shumpei116 <>
2021-08-09 07:52:08 +0900

    rubocopが通るようにコードを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @stores = @user.stores.order(created_at: :desc).page(params[:index_page]).per(8)
-    @favorite_stores = @user.favorite_stores.order(created_at: :desc).page(params[:favorite_page]).per(8).includes(:user)
+    @favorite_stores = @user.favorite_stores.order(created_at: :desc).page(params[:favorite_page]).per(8)
+                            .includes(:user)
   end
 end
