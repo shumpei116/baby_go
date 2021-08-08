9204fa4626c3219b0e349fc30f0f7657bef9fe0d
shumpei116 <>
2021-08-08 10:29:20 +0900

    ページネーションの為のparamsを変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @stores = @user.stores.order(created_at: :desc).page(params[:page]).per(8)
-    @favorite_stores = @user.favorite_stores.order(created_at: :desc).page(params[:page]).per(8)
+    @stores = @user.stores.order(created_at: :desc).page(params[:index_page]).per(8)
+    @favorite_stores = @user.favorite_stores.order(created_at: :desc).page(params[:favorite_page]).per(8)
   end
 end
