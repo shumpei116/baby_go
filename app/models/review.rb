489c5a3433f9436adb57ac121db4d9d622205317
shumpei116 <>
2021-08-13 11:33:50 +0900

    バリデーションの記述ミスを修正

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,7 +5,7 @@ class Review < ApplicationRecord
 
   validates :store_id, presence: true
   validates :user_id, presence: true
-  validates :rate, presence: true, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 1 }
+  validates :rating, presence: true, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 1 }
   validates :comment, presence: true
   validates :store_id, uniqueness: { scope: :user_id }
 end
