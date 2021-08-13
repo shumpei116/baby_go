4b840333db9fee81435b18cefa54986255f13f19
shumpei116 <>
2021-08-14 07:37:17 +0900

    数値のバリデーションを0.5以上に修正

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,7 +5,7 @@ class Review < ApplicationRecord
 
   validates :store_id, presence: true
   validates :user_id, presence: true
-  validates :rating, presence: true, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 1 }
-  validates :comment, presence: true
+  validates :rating, presence: true, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 0.5 }
+  validates :comment, presence: true, length: { maximum: 140 }
   validates :store_id, uniqueness: { scope: :user_id }
 end
