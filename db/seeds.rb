e69d6329885fbb78517bf80a5ad81f44309cb169
shumpei116 <>
2021-08-13 12:23:39 +0900

    レビューのサンプルデータを生成

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,3 +68,13 @@ favorite_users.each do |favorite_user|
     favorite_user.favorites.create!(store_id: store.id)
   end
 end
+
+# レビューデータを生成
+reviewed_users = users[0..4]
+reviewed_stores = Store.all[5..20]
+favorite_users.each do |reviewed_user|
+  reviewed_stores.each_with_index do |store, index|
+    comment = "#{rand(1..15)}番いいところでした！！"
+    reviewed_user.reviews.create!(store_id: store.id, rating: rand(1..5), comment: comment)
+  end
+end
