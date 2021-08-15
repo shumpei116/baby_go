d8be63e59d2ae0c140f96a127de63b3fc24a31bc
shumpei116 <>
2021-08-16 05:49:20 +0900

    FactoryBotでstoreをbuildした時の関連データの挙動をbuildからcreateに変更

--- spec/factories/stores.rb
+++ spec/factories/stores.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :store do
-    association :user
+    association :user, strategy: :create
     name { '赤ちゃんの本舗' }
     introduction { '授乳室とおむつ交換スペースが完備！　綺麗で広くてとっても利用しやすいです' }
     postcode { '3120005' }
