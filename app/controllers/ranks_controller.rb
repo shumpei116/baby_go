09042a12305acee842c5d7f59f7188dfb4722d40
shumpei116 <>
2021-08-15 10:27:58 +0900

    施設をレビューの平均点順に取得する為、コントローラーに追加

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,4 +1,8 @@
 class RanksController < ApplicationController
   def index
+    rank_stores = Store.left_joins(:reviews).includes(:reviews).distinct.sort_by(&:average_rating)
+                       .reverse
+
+    @rank_stores = Kaminari.paginate_array(rank_stores).page(params[:rank_page]).per(10)
   end
 end
