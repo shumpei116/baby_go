c35a549adf6faefff889bd09697763e20a4ca0ed
shumpei116 <>
2021-08-31 11:26:54 +0900

    docker環境でheadresschromeを使用してsystem_specを動作させる為、capybara.rbを編集

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,9 +1,20 @@
 RSpec.configure do |config|
+  Capybara.register_driver :selenium_chrome do |app|
+    options = ::Selenium::WebDriver::Chrome::Options.new
+
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox') # chrootで隔離された環境(Sandbox)での動作を無効
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1400,1400')
+
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  end
+
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
 
   config.before(:each, type: :system, js: true) do
-    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+    driven_by :selenium_chrome
   end
 end
