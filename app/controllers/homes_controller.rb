abb93f0f07e1a6481954d35b420f62aca5dbf001
shumpei116 <>
2021-09-14 06:25:02 +0900

    トップページにレビューランキングを追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,8 @@
 class HomesController < ApplicationController
+  MAX_RANK_STORE_COUNT = 3
   def top
     @stores = Store.all
     gon.stores = @stores
+    @rank_store = Store.average_score_rank.take(MAX_RANK_STORE_COUNT)
   end
 end
