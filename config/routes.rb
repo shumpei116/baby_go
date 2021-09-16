72ce460d8d5330b3b8e44124ead8a5cafb2d03a7
shumpei116 <>
2021-09-17 05:31:34 +0900

    ゲストユーザー作成用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
   root 'homes#top'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
   }
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
   resources :users, only: [:index, :show]
+
   resources :stores do
     resource :favorite, only: [:create, :destroy]
     resource :review, only: [:create, :edit, :update, :destroy]
   end
+
   resources :ranks, only: [:index]
   resources :spots, only: [:index]
 end
