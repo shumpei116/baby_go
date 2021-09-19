aac864f7831490aa65d51fea5c1f2fb8b688d762
shumpei116 <>
2021-09-19 16:11:47 +0900

    production.key参照の為のコードを追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :branch, ENV['BRANCH'] || "main"
 set :nginx_config_name, "#{fetch(:application)}.conf"
 set :nginx_sites_enabled_path, "/etc/nginx/conf.d"
 append :linked_files, "config/master.key"
+append :linked_files, 'config/credentials/production.key'
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "node_modules"
 
 
