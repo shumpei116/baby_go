4d400bcccc7323b4acacfca6c0052c321bee0d9f
shumpei116 <>
2021-09-22 15:14:32 +0900

    circleCIから自動デプロイする為、kyes:の参照先を変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 server "35.76.64.33", user: "shumpei", roles: %w{app db web}
 
 set :ssh_options, {
-  keys: %w(~/.ssh/Baby_Go.pem),
+  keys: (ENV['PRODUCTION_SSH_KEY']),
   forward_agent: true,
   auth_methods: %w(publickey),
 }
