de529c2f01fc73b58028194215a5ae846fcd4132
shumpei116 <>
2021-09-23 10:02:40 +0900

    circleCI自動デプロイの為、rubyバージョンを指定

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.16.0"
 
 set :application, "Baby_Go"
 set :repo_url, "git@github.com:shumpei116/baby_go.git"
-set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_ruby, '2.7.4'
 set :branch, ENV['BRANCH'] || "main"
 set :nginx_config_name, "#{fetch(:application)}.conf"
 set :nginx_sites_enabled_path, "/etc/nginx/conf.d"
