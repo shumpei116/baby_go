2b06996c9355128a454be974111fc721db98fcb4
shumpei116 <>
2021-09-27 18:40:22 +0900

    メタタグの設定を記述

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,29 @@ module ApplicationHelper
       "#{page_title} - #{BASE_TITLE}"
     end
   end
+
+  def default_meta_tags
+    {
+      site: 'Baby_Go',
+      title: 'Baby_Go',
+      reverse: true,
+      separator: '|',
+      description: 'Baby_Gpは授乳室やおむつ交換スペースが完備された施設、赤ちゃんと一緒に入れる飲食店などの情報を共有・検索するためのWEBサービスです',
+      keywords: 'Baby_Go,ベイビーゴー,赤ちゃんがいても,赤ちゃんと',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      og: {
+        site_name: 'Baby_Go',
+        title: 'Baby_Go',
+        description: 'Baby_Gpは授乳室やおむつ交換スペースが完備された施設、赤ちゃんと一緒に入れる飲食店などの情報を共有・検索するためのWEBサービスです',
+        type: 'website',
+        url: request.original_url,
+        image: image_pack_tag('media/images/logo.png'),
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary'
+      }
+    }
+  end
 end
