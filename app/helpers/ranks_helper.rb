a634df05c65e4de3e8ae39594140e1a8ca36af7b
shumpei116 <>
2021-10-06 05:49:46 +0900

    順位表示のヘルパーを順位取得と表示に分割

--- app/helpers/ranks_helper.rb
+++ app/helpers/ranks_helper.rb
@@ -1,7 +1,9 @@
 module RanksHelper
-  def check_and_show_rank(stores, store)
-    ranking = stores.index(store) + 1
+  def fetch_rank(total_value, value)
+    total_value.index(value) + 1
+  end
 
+  def show_rank(ranking)
     if ranking >= 1 && ranking <= 3
       image_pack_tag("media/images/home_rank_#{ranking}.png", size: '130x80', alt: "ランキング#{ranking}位画像")
     else
